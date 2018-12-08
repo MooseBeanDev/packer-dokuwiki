8fa5cddec502a6941c60ba5eb7ebc643774bdff3
moosebeandev <bmccown93@gmail.com>
2018-12-08 16:44:45 -0600

    Set file permissions after restoring dokuwiki. Broke vars into separate file.

--- restore-dokuwiki.sh
+++ restore-dokuwiki.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 cd /var/www/html/wiki && sudo git pull
+sudo chown -R apache:apache /var/www/html/wiki
+sudo chmod 755 -R /var/www/html/wiki
+sudo chmod 700 -R /var/www/html/wiki/data
+sudo chmod 700 -R /var/www/html/wiki/conf
